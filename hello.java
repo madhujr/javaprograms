73d4037021274ba9dbece91ccced6301cd3fcb9f
madhujr <50518028+madhujr@users.noreply.github.com>
2019-05-11 17:06:41 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Hello{
+public static void main(String[] qwe){
+Scanner Scan=new Scanner(System.in);
+System.out.print("enter the value of N:");
+int N=Scan.nextInt();
+int x=1;
+while(x<=N){
+System.out.println("hello");
+x++;
+}
+}
+}
